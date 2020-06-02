da4b2ad5c4738794f5af7e95c4a1055de2f51dd7
Cigin Koshy <cigin.koshy@schibsted.com>
2020-06-02 10:49:59 +0200

    adding restart shell script

---
+++ restart.sh
@@ -0,0 +1,4 @@
+curl -X DELETE "https://api.heroku.com/apps/peaceful-hollows-29049/dynos" \                                                                                                                  
+  --user "cigink@gmail.com:6d983a40-6062-44a7-b7da-3e1fe93058fe" \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3"
